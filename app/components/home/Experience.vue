91a0ddbcdbc5d0d8657dff425da915e5b4244c75
Jacob Padley <jacob5180@hotmail.co.uk>
2026-02-25 12:39:58 +0000

    feat: links from timeline to experience

--- app/components/home/Experience.vue
+++ app/components/home/Experience.vue
@@ -15,7 +15,16 @@ const { data: items } = await useAsyncData(() => queryCollection('experience').a
           date: 'text-md',
         }"
         :items
-      />
+      >
+      <template #title="{ item }">
+        <ULink :to="item.path" class="text-neutral hover:underline">
+          <span>
+            {{ item.title }}
+            <UIcon name="i-lucide-arrow-right" class="ml-2 pb-2 align-middle" />
+          </span>
+        </ULink>
+      </template>
+    </UTimeline>
     </template>
   </UPageSection>
 </template>
