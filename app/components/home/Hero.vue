92dd7cb11e713b4a232f8199d45fc2d675d08d28
Jacob Padley <jacob5180@hotmail.co.uk>
2026-03-05 22:45:51 +0000

    feat: responsive mobile About Me hero

--- app/components/home/Hero.vue
+++ app/components/home/Hero.vue
@@ -10,10 +10,17 @@ const props = defineProps<{
       <slot mdc-unwrap="p" />
     </template>
 
-    <img :src="props.image" class="w-50 h-50 aspect-square rounded-full" />
+    <img :src="props.image" class="hidden lg:block w-50 h-50 aspect-square rounded-full" />
 
     <template #description>
-      <slot name="description" />
+      <div class="flex flex-col gap-6">
+        <img
+          :src="props.image"
+          class="block lg:hidden w-42 h-42 aspect-square rounded-full self-start"
+        />
+
+        <slot name="description" />
+      </div>
     </template>
   </UPageHero>
 </template>
